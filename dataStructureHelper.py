d096795961188b32120d3d176c749f57f4948c05
jonas_e <emile.jonas@epitech.eu>
2018-05-30 10:36:39 +0200

    parse csv into usable datastructure

---
+++ dataStructureHelper.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+ID = 0
+TITLE = 1
+TYPE = 2
+SOURCE = 3
+EPISODES = 4
+DATE = 5
+DURATION = 6
+RATING = 7
+SCORE = 8
+RANK = 9
+SCORED_BY = 10
+POPULARITY = 11
+MEMBERS = 12
+FAVORITES = 13
+RELATED = 14
+GENRE = 15
+WATCHING = 16
+COMPLETED = 17
+ON_HOLD = 18
+DROPPED = 19
+PLAN_TO_WATCH = 20
+TOTAL = 21
+
+class Anime(Enum):
+    ID = 0
+    TITLE = 1
+    TYPE = 2
+    SOURCE = 3
+    EPISODES = 4
+    AIRED = 5
+    DURATION = 6
+    RATING = 7
+    SCORE = 8
+    RANK = 9
+    SCORED_BY = 10
+    POPULARITY = 11
+    MEMBERS = 12
+    FAVORITES = 13
+    RELATED = 14
+    GENRE = 15
+    WATCHING = 16
+    COMPLETED = 17
+    ON_HOLD = 18
+    DROPPED = 19
+    PLAN_TO_WATCH = 20
+    TOTAL = 21
--- parseCSV.py
+++ parseCSV.py
@@ -1,9 +1,32 @@
 from pprint import pprint
 import csv
+from dateutil.parser import parse
+from dataStructureHelper import *
 
-results = []
-with open("test.csv") as csvfile:
-    reader = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    for row in reader:
-        results.append(row)
-pprint(results)
+kvStore = {}
+ID = 0
+
+def getID(string):
+    global kvStore, ID
+    if string not in kvStore:
+        kvStore[string] = ID
+        ID += 1
+    return kvStore[string]
+
+def parseAnime(filename):
+        results = []
+        with open(filename) as csvfile:
+            reader = csv.reader(csvfile, quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
+            for row in reader:
+                row[TYPE] = getID(row[TYPE])
+                row[SOURCE] = getID(row[SOURCE])
+                row[DATE] = parse(row[DATE])
+                row[GENRE] = row[GENRE].strip(' ')[:-1].split(',')
+                for i, genre in enumerate(row[GENRE]):
+                    row[GENRE][i] = getID(genre)
+                row[DURATION] = getID(row[DURATION])
+                row[RATING] = getID(row[RATING])
+                results.append(row)
+        pprint(results)
+
+parseAnime("anime.csv")
